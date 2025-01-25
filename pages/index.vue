c9b8d79a2e6ed4877aee339453cfde2ee553777c
Erich Grassmann <erich@grassmann.at>
2025-01-25 09:54:21 +0100

    Bis Nuxt 5

---
+++ pages/index.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+        <h2>Home</h2>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam hic quos obcaecati minus dolore? Quia fugit ducimus nobis sed! Porro consequuntur quae nostrum at saepe mollitia inventore corporis pariatur est!</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam hic quos obcaecati minus dolore? Quia fugit ducimus nobis sed! Porro consequuntur quae nostrum at saepe mollitia inventore corporis pariatur est!</p>
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+    h2 {
+        margin-bottom: 20px;
+        font-size: 36px;
+    }
+
+    p {
+        margin: 20px;
+    }
+</style>
---
+++ pages/products/[id].vue
@@ -0,0 +1,14 @@
+<template>
+    <div>
+        <p>Product details for {{ id }}</p>
+        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Accusantium officiis est, soluta quidem veniam magnam error repellat molestiae. Fugiat voluptate maxime debitis facilis ab! Veritatis amet itaque doloremque dignissimos dolore?</p>
+    </div>
+</template>
+
+<script setup>
+    const { id } = useRoute().params
+</script>
+
+<style scoped>
+
+</style>
